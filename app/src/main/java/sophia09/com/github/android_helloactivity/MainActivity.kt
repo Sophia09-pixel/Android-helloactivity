b1f9342c460f29fb5469f61cc6684d1587d01069
Sophia <sophia.dev0903@gmail.com>
2025-03-13 22:26:27 -0300

    feat: aula de ciclo de vida de activity

--- app/src/main/java/sophia09/com/github/android_helloactivity/MainActivity.kt
+++ app/src/main/java/sophia09/com/github/android_helloactivity/MainActivity.kt
@@ -1,20 +1,12 @@
 package sophia09.com.github.android_helloactivity
 
+import DebugActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import sophia09.com.github.android_helloactivity.ui.theme.AndroidhelloactivityTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : DebugActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
     }
 }
